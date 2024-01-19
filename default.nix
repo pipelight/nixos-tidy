0067768053afdba6d045361849da9e1e4ac252e1
areskill <areskul@areskul.com>
2024-01-19 15:11:15 +0100

    working mkApply

--- default.nix
+++ default.nix
@@ -3,51 +3,40 @@
   pkgs,
   lib,
   input,
-  cfg,
 }: {
-  # A function to merge home files and apply them to a list of users
-  # merge_homes [ homeManagerModule1, homeManagerModule2 ]
-  mkMergeHomes = home_files: inheritage:
-  # For every home-manager module paths
-  # example: "home.nix"
-  # import and pass args
-    (map
-    (
-      rel_path: (import rel_path inheritage)
-    )
-    home_files);
-
-  # Apply home modules to user list
-  mkApplyHomes = home_modules: users: [
+  # A Function to apply home.nix home-manager
+  # configurations to multiple users
+  # Args:
+  # - home_modules; a list of home-manager modules "home.nix" files,
+  # - apply_on_users; a list of usernames
+  # Return
+  # - a list of modules
+  # Usage:
+  # ```nix
+  #  imports = [] ++ mkApplyHomes [(import ./a/home.nix)] ["anon"];
+  # ```
+  mkApplyHomes = home_modules: apply_on_users: let
+    homeManagerModule = inputs.home-manager.nixosModules.home-manager;
+  in [
     homeManagerModule
     {
-      home-manager = [
+      home-manager =
         {
           useGlobalPkgs = true;
-          extraSpecialArgs = {inherit system inputs;};
+          extraSpecialArgs = {inherit inputs;};
         }
-        (listToAttrs (
-          # For every user name
-          # example "anon"
-          map (u: {
+        // builtins.listToAttrs (
+          builtins.map (u: {
             name = "users";
             value = {
               ${u} = {
                 home.stateVersion = "24.05";
-                imports =
-                  # For every home-manager module paths
-                  # example: "home.nix"
-                  home_modules;
+                imports = home_modules;
               };
             };
           })
-          cfg.users
-        ))
-      ];
+          apply_on_users
+        );
     }
-  ];
-
-  # Apply default modules and provide user list for internal usage
-  mkApplyModules = default_modules: users: [
   ];
 }
