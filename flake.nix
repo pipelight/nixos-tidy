9aeeffa8d9b8f5f41f1ba3075c6e107d03c8d680
areskill <areskul@areskul.com>
2024-01-20 17:50:26 +0100

    stuff

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
                   home-manager =
                     {
                       useGlobalPkgs = true;
-                      extraSpecialArgs = {inherit inputs system;};
+                      extraSpecialArgs = {inherit pkgs inputs system;};
                     }
                     // builtins.listToAttrs (
                       builtins.map (u: {
