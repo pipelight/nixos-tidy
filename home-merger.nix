7b5351da5fa5158cdd13e0bef1333aff5e7a05c4
areskill <areskul@areskul.com>
2024-03-16 11:53:46 +0100

    test

--- home-merger.nix
+++ home-merger.nix
@@ -6,61 +6,62 @@
 }:
 with lib; let
   homeManagerModule = inputs.home-manager.nixosModules.home-manager;
-  cfg = config.home-merger;
+  # cfg = config.home-merger;
 in {
   # Set the module options
-  options.home-merger = {
-    enable = mkEnableOption ''
-      Wether to enable thos modules for the specified users'.
-    '';
-
-    users = mkOption {
-      type = with types; listOf str;
-      default = [];
-      example = literalExpression "[\"alice\",\"bob\"]";
-      description = ''
-        The name of users for whome to add this module.
+  options = {
+    home-merger = {
+      enable = mkEnableOption ''
+        Wether to enable thos modules for the specified users'.
       '';
-    };
-    extraSpecialArgs = mkOption {
-      type = with types; attrs;
-      default = {};
-      example = literalExpression "{ inherit emacs-overlay; }";
-      # type = with types; listOf inferred;
-      description = ''
-        Extra `specialArgs` passed to Home Manager. This
-        option can be used to pass additional arguments to all modules.
-      '';
-    };
-    modules = mkOption {
-      type = with types; listOf raw;
-      default = [];
-      example = literalExpression "[ ./home.nix, otherModule ]";
-      description = ''
-        Modules to add to the user configuration.
-      '';
+      users = mkOption {
+        type = with types; listOf str;
+        default = [];
+        example = literalExpression "[\"alice\",\"bob\"]";
+        description = ''
+          The name of users for whome to add this module.
+        '';
+      };
+      extraSpecialArgs = mkOption {
+        type = with types; attrs;
+        default = {};
+        example = literalExpression "{ inherit emacs-overlay; }";
+        # type = with types; listOf inferred;
+        description = ''
+          Extra `specialArgs` passed to Home Manager. This
+          option can be used to pass additional arguments to all modules.
+        '';
+      };
+      modules = mkOption {
+        type = with types; listOf raw;
+        default = [];
+        example = literalExpression "[ ./home.nix, otherModule ]";
+        description = ''
+          Modules to add to the user configuration.
+        '';
+      };
     };
   };
-  imports = [
-    homeManagerModule
-    {
-      home-manager =
-        {
-          useGlobalPkgs = false;
-          extraSpecialArgs = cfg.extraSpecialArgs;
-        }
-        // builtins.listToAttrs (
-          builtins.map (u: {
-            name = "users";
-            value = {
-              ${u} = {
-                home.stateVersion = "24.05";
-                imports = cfg.modules;
-              };
-            };
-          })
-          cfg.users
-        );
-    }
-  ];
+  # imports = [
+  #   homeManagerModule
+  #   {
+  #     home-manager =
+  #       {
+  #         useGlobalPkgs = false;
+  #         extraSpecialArgs = cfg.extraSpecialArgs;
+  #       }
+  #       // builtins.listToAttrs (
+  #         builtins.map (u: {
+  #           name = "users";
+  #           value = {
+  #             ${u} = {
+  #               home.stateVersion = "24.05";
+  #               imports = cfg.modules;
+  #             };
+  #           };
+  #         })
+  #         cfg.users
+  #       );
+  #   }
+  # ];
 }
